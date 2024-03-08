c0b2bbc12914f76301144e4129d67604c567a61c
Alex <yevsyukovaleks@gmail.com>
2024-03-08 15:19:35 +0300

    Spring Initializr

---
+++ src/test/java/ru/hse/coursework/geolesson/GeolessonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.coursework.geolesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
